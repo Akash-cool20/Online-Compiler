e65daeb57a4eac3588ec9b968a0f65ea862ad605
Akash <akash.eduthrill2025@gmail.com>
2024-06-21 19:32:05 +0530

    setup completed

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+     <Button variant="ghost">test </Button>
+     
+    </>
+  )
+}
+
+export default App
